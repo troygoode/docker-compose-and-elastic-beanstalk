f94c2767a00be4e6e470706527d31bbf9259208d
Troy Goode <troygoode@gmail.com>
2016-08-19 21:19:29 -0700

    typo

--- api/server.js
+++ api/server.js
@@ -10,5 +10,5 @@ app.get('/api', function (req, res) {
 })
 
 app.listen(3001, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3001!')
 })
